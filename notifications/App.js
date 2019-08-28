3fbcf96f4c33ae7a0b811a1d7cb326c67ef6089a
Adnan <ayunus@ucsc.edu>
2019-08-28 02:49:11 -0700

    EntryScreen Configuration

--- notifications/App.js
+++ notifications/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import EntryScreen from './components/EntryScreen';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <EntryScreen />
   );
 }
 
